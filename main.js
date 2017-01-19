b8e3447f5313b3ebecfe9874f6f24c03553559c9
Jared Fuller <jaredshane@gmail.com>
2017-01-19 10:37:26 -0600

    added partials folder and set up route for it

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hey");
+angular
+  .module('angularWeather', ['ngRoute'])
+  .config(($routeProvider, $locationProvider) => {
+    $locationProvider.hashPrefix('')
+    $routeProvider
+      .when('/', {
+        controller: 'RootCtrl',
+        templateUrl: 'partials/root.html'
+      })
+      .when('/weather/:zipcode', {
+        controller: 'WeatherCutler',
+        templateUrl: 'partials/weather.html'
+      })
+  })
+  .controller('RootCtrl', function () {
+    console.log("hey sup");
+  })
