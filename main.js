417d99986391279ab94a98d4beccb16e5fa1289c
Jared Fuller <jaredshane@gmail.com>
2017-01-19 13:22:37 -0600

    boom done

--- main.js
+++ main.js
@@ -8,10 +8,40 @@ angular
         templateUrl: 'partials/root.html'
       })
       .when('/weather/:zipcode', {
-        controller: 'WeatherCutler',
+        controller: 'WeatherCtrl',
         templateUrl: 'partials/weather.html'
       })
   })
-  .controller('RootCtrl', function () {
+  .controller('RootCtrl', function ($scope, $location) {
+    $scope.goToWeather = () => {
+      //chnage the URL
+      // location.href = `/#/weather/${$scope.zip}`
+      $location.url(`/weather/${$scope.zip}`)
+    }
     console.log("hey sup");
+  })
+  .controller('WeatherCtrl', function ($routeParams, $scope, weatherFactory) {
+    weatherFactory
+      .getWeather($routeParams.zipcode)
+      .then((weather) => {
+        $scope.temperature = weather.temp
+        $scope.city = weather.city
+        $scope.icon = weather.icon
+      })
+    // console.log('hey dis da weather')
+  }).factory('weatherFactory', ($http) => {
+      return {
+        getWeather(zipcode) {
+          return $http
+            .get(`http://api.wunderground.com/api/33539797879b6485/conditions/q/${zipcode}.json`)
+            .then((response)=> {
+              console.log(response)
+              return {
+                  temp: response.data.current_observation.temp_f,
+                  city: response.data.current_observation.display_location.full,
+                  icon: response.data.current_observation.icon_url
+              }
+            })
+        }
+      }
   })
